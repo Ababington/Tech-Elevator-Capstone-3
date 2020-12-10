d7bf470329361889e943db5862cbe34c6d77bef2
Vinny Bucci <vinnybucci10@gmail.com>
2020-12-10 15:22:37 -0500

    added changes to office model

--- api/Capstone/Models/Office.cs
+++ api/Capstone/Models/Office.cs
@@ -8,9 +8,17 @@ namespace Capstone.Models
     public class Office
     {
         public int OfficeId { get; set; }
-        public int AddressId { get; set; }
+      //  public int AddressId { get; set; }
         public int Phone { get; set; }
         public int OfficeHours { get; set; }
+        public string OfficeName { get; set; }
+       // public int AddressId { get; set; }
+        public int UserId { get; set; }
+        public string StreetAddress { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
 
     }
 }
