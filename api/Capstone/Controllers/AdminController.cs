ec324ed5d960b3f32e3b241635b9ed83f5ad4cff
Vinny Bucci <vinnybucci10@gmail.com>
2020-12-11 12:52:38 -0500

    Lots of changes ask Rob

--- api/Capstone/Controllers/AdminController.cs
+++ api/Capstone/Controllers/AdminController.cs
@@ -10,20 +10,27 @@ namespace Capstone.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class AdminController:ControllerBase
+    public class AdminController : ControllerBase
     {
         private readonly IUserDAO userDAO;
         private readonly IOfficeDAO officeDAO;
         private readonly IAddressDAO addressDAO;
-        public AdminController(IUserDAO _userDAO, IOfficeDAO _officeDAO, IAddressDAO _addressDAO)
+        private readonly IOfficeAddressDAO officeAddressDAO;
+        public AdminController(IUserDAO _userDAO, IOfficeDAO _officeDAO, IAddressDAO _addressDAO, IOfficeAddressDAO _officeAddressDAO)
         {
             userDAO = _userDAO;
             officeDAO = _officeDAO;
             addressDAO = _addressDAO;
+            officeAddressDAO = _officeAddressDAO;
         }
 
+        [HttpGet]
+        public ActionResult<string> BaseAdminTest()
+        {
+            return Ok("reached admin");
+        }
 
-        [HttpGet("/getPendingDoctors")]
+        [HttpGet("getPendingDoctors")]
         public ActionResult<List<User>> GetPendingDoctors()
         {
             try
@@ -31,17 +38,41 @@ namespace Capstone.Controllers
                 List<User> pendingDoctors = userDAO.GetPendingDoctors();
                 return pendingDoctors;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw new NotImplementedException("This method is not implemented");
             }
         }
-        [HttpPost("/createNewOffice")]
+        [HttpPost("createNewOffice")]
         public ActionResult<Office> CreateNewOffice(Office office)
         {
             try
             {
-                addressDAO.CreateNewOfficeAddress(office);
+                int? newOfficeId = officeDAO.CreateNewOffice(office);
+                if (newOfficeId != null)
+                {
+                   int newOfficeIdNotNull = (int)(newOfficeId); 
+                    
+                    int? newAddressId = addressDAO.CreateNewOfficeAddress(office);
+
+                    if (newAddressId != null)
+                    {
+                        int newAddressIdNotNull = (int)(newAddressId);
+                        officeAddressDAO.AddOfficeAddress( newOfficeIdNotNull, newAddressIdNotNull);
+                        office.OfficeId = newOfficeIdNotNull;
+                        return Ok(office);
+                    }
+                    else
+                    {
+                        //if address sql fails
+                        return BadRequest();
+                    }
+                }
+                else
+                {
+                    //if office sql fails
+                    return BadRequest();
+                }
 
             }
             catch (Exception e)
--- api/Capstone/Models/Office.cs
+++ api/Capstone/Models/Office.cs
@@ -8,12 +8,8 @@ namespace Capstone.Models
     public class Office
     {
         public int OfficeId { get; set; }
-      //  public int AddressId { get; set; }
         public int Phone { get; set; }
-        public int OfficeHours { get; set; }
-        public string OfficeName { get; set; }
-       // public int AddressId { get; set; }
-        public int UserId { get; set; }
+        public string Name { get; set; }
         public string StreetAddress { get; set; }
         public string StreetAddress2 { get; set; }
         public string City { get; set; }
