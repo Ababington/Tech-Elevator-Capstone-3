b8d571dd8396984ff47ee2baed7408f377d70f8e
August Babington <augustbabington@gmail.com>
2020-12-12 12:29:24 -0500

    pulling

--- api/Capstone/Models/Office.cs
+++ api/Capstone/Models/Office.cs
@@ -8,11 +8,7 @@ namespace Capstone.Models
     public class Office
     {
         public int OfficeId { get; set; }
-<<<<<<< HEAD
-        public int PhoneNumber { get; set; }
-=======
         public string Phone { get; set; }
->>>>>>> 3b20e2d5cbf52d858f710b8d37bc84c609b3baf0
         public string Name { get; set; }
         public string StreetAddress { get; set; }
         public string StreetAddress2 { get; set; }
