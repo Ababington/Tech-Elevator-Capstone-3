872dc705694714da976342bf4cdcc8fb0e4a5c3e
August Babington <augustbabington@gmail.com>
2020-12-12 14:48:43 -0500

    changed phone

--- vue/src/components/AdminPendingDoctors.vue
+++ vue/src/components/AdminPendingDoctors.vue
@@ -1,27 +1,46 @@
 <template>
-    <div>
-        <div v-for="doctor in doctors" v-bind:key="doctor.id">
-          {{doctor.username}}
-          <!-- {{doctor.status}} -->
-        </div>
-    </div>
+  <div>
+    <form v-on:submit.prevent="ApproveDoctorUser()">
+      <div
+        v-for="doctor in doctors"
+       
+        v-bind:key="doctor.id"
+      >
+        {{ doctor.username }}
+       
+        <button type="submit" v-on:click="user.user_role = 'approve'" >Approve Doctor Status</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import adminService from '../services/AdminService';
+import adminService from "../services/AdminService";
 
 export default {
   data() {
     return {
+      user: {
+        user_role: "",
+      },
       doctors: [],
     };
   },
-  created() {
-        adminService.getPendingDoctors()
-        .then(response => {
-            this.doctors=response.data;
-        })
-    }
+  methods: {
+    ApproveDoctorUser() {
+      adminService.ApproveDoctorUser(this.doctor).then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          alert("Doctor Status Confirmed");
+          this.$router.push("adminHome");
+        }
+      });
+    },
+    created() {
+      adminService.GetPendingDoctors().then((response) => {
+        this.doctors = response.data;
+      });
+    },
+  },
 };
 </script>
 
