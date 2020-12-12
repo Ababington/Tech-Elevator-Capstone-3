645215ca61b6eb09b7140c3c596d6cb5bd1f1b84
Vinny Bucci <vinnybucci10@gmail.com>
2020-12-12 11:56:02 -0500

    Changes to database and added some models for days of the week

---
+++ api/Capstone/Models/DayWednesday.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class DayWednesday
+    {
+        public string Wednesday { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
