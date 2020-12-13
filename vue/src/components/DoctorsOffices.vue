f77b9871fee8835e4b1b2660fe2388f80711a47c
Nick Kopp <Li77l3n@gmail.com>
2020-12-12 20:11:18 -0500

    worked on patients interface

---
+++ vue/src/components/DoctorsOffices.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+<div v-for="office in office" v-bind:key="office.id">
+    {{office.name}}
+    {{'info to display'}}
+
+</div>
+
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        offices: []
+    }
+}
+
+}
+</script>
+
+<style>
+
+</style>
