bfa2f381ca4a5d2ade05a62f3d04a830af912154
Nick Kopp <Li77l3n@gmail.com>
2020-12-13 14:27:02 -0500

    worked on doctors offices for patient view

--- vue/src/components/DoctorsOffices.vue
+++ vue/src/components/DoctorsOffices.vue
@@ -1,19 +1,30 @@
 <template>
 <div>
-<div v-for="office in office" v-bind:key="office.id">
-    {{office.name}}
-    {{'info to display'}}
+<div v-for="office in offices" v-bind:key="office.id" v-bind:office="office">
+   <office-card>
 
 </div>
 
 </template>
 
 <script>
+import PatientService from '../services/PatientService'
+import officeCard from '../components/OfficeCard'
 export default {
+    components: {
+        officeCard
+    },
+
 data(){
     return{
         offices: []
     }
+},
+
+created(){
+    PatientService.GetDoctorsOffices(this.router.params).then((response) => {
+        this.offices = response.data;
+    })
 }
 
 }
