4451ceedbfe7e3a7fb15b811528c7c31331f577c
August Babington <augustbabington@gmail.com>
2020-12-15 11:12:07 -0500

    Css

--- vue/src/views/OfficeInfo.vue
+++ vue/src/views/OfficeInfo.vue
@@ -32,7 +32,7 @@ export default {
 }
 </script>
 
-<style>
+<style >
 
 .doctorOffice{
   font-family:"Courier", impact, monospace ;
--- vue/src/views/PatientOfficeInfo.vue
+++ vue/src/views/PatientOfficeInfo.vue
@@ -27,7 +27,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .doctorOffice{
   font-family:"Courier", impact, monospace ;
