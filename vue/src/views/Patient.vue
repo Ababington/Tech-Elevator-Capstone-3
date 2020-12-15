2675c61e1275243900cc152e5cf9f63a2c71cb2e
Nick Kopp <Li77l3n@gmail.com>
2020-12-15 11:15:04 -0500

    added buttons

--- vue/src/views/Patient.vue
+++ vue/src/views/Patient.vue
@@ -1,6 +1,7 @@
 <template>
 <div class='patient'>
-     <div v-for="patient in patient" v-bind:key="patient.id"></div>
+    <div v-for="patient in patient" v-bind:key="patient.id"></div>
+    <patient-buttons/>
     <button class="appointment" v-on:click="appointments">Request appointment</button>
     <router-link v-bind:to="{name: 'patientofficeInfo'}"><button class="patientOfficeInfo" v-on:click="patientOfficeInfo">Office Info</button></router-link>
     <button class="reviews" v-on:click="reviews">Reviews</button>
@@ -11,10 +12,12 @@
 
 <script>
 import appointmentForm from '../components/AppointmentForm';
+import patientButtons from '../components/PatientButtons';
 
 export default {
     components:{
-        appointmentForm
+        appointmentForm,
+        patientButtons
     }
 
 }
