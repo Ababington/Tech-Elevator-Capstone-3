e84de91f343911cfc1d9adaa286e9295366246f8
Nick Kopp <Li77l3n@gmail.com>
2020-12-15 14:31:17 -0500

    added a notification

---
+++ vue/src/components/NotificationMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        You have a new appointment!
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
